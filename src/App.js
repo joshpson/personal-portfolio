877f932d8d23c407f9e31614f34f70688874e482
joshpson <jpearson846@gmail.com>
2018-07-11 15:12:11 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from "react";
+import { Container, Menu, Image, Dropdown } from "semantic-ui-react";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">This will be my portfolio site in time</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container>
+        <Menu borderless>
+          <Container text>
+            <Menu.Item>
+              <Image size="mini" src="/logo.png" />
+            </Menu.Item>
+            <Menu.Item header>Project Name</Menu.Item>
+            <Menu.Item as="a">Blog</Menu.Item>
+            <Menu.Item as="a">Articles</Menu.Item>
+
+            <Menu.Menu position="right">
+              <Dropdown text="Dropdown" pointing className="link item">
+                <Dropdown.Menu>
+                  <Dropdown.Item>List Item</Dropdown.Item>
+                  <Dropdown.Item>List Item</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Header>Header Item</Dropdown.Header>
+                  <Dropdown.Item>
+                    <i className="dropdown icon" />
+                    <span className="text">Submenu</span>
+                    <Dropdown.Menu>
+                      <Dropdown.Item>List Item</Dropdown.Item>
+                      <Dropdown.Item>List Item</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown.Item>
+                  <Dropdown.Item>List Item</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Menu.Menu>
+          </Container>
+        </Menu>
+      </Container>
     );
   }
 }
