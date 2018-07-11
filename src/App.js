a764ef4c2fbcd1f3b172f76439f9a149839c7c52
joshpson <jpearson846@gmail.com>
2018-07-11 15:33:23 -0400

    header and image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Container, Menu, Image, Dropdown } from "semantic-ui-react";
 
-import logo from "./logo.svg";
+import headshot from "./images/josh.jpg";
 import "./App.css";
 
 class App extends Component {
@@ -9,35 +9,11 @@ class App extends Component {
     return (
       <Container>
         <Menu borderless>
-          <Container text>
-            <Menu.Item>
-              <Image size="mini" src="/logo.png" />
-            </Menu.Item>
-            <Menu.Item header>Project Name</Menu.Item>
-            <Menu.Item as="a">Blog</Menu.Item>
-            <Menu.Item as="a">Articles</Menu.Item>
-
-            <Menu.Menu position="right">
-              <Dropdown text="Dropdown" pointing className="link item">
-                <Dropdown.Menu>
-                  <Dropdown.Item>List Item</Dropdown.Item>
-                  <Dropdown.Item>List Item</Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Header>Header Item</Dropdown.Header>
-                  <Dropdown.Item>
-                    <i className="dropdown icon" />
-                    <span className="text">Submenu</span>
-                    <Dropdown.Menu>
-                      <Dropdown.Item>List Item</Dropdown.Item>
-                      <Dropdown.Item>List Item</Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown.Item>
-                  <Dropdown.Item>List Item</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </Menu.Menu>
-          </Container>
+          <Menu.Item header>Josh Pearson</Menu.Item>
+          <Menu.Item as="a">Blog</Menu.Item>
+          <Menu.Item as="a">Articles</Menu.Item>
         </Menu>
+        <Image src={headshot} size="small" circular />
       </Container>
     );
   }
